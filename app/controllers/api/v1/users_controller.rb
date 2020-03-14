71df5a74be1ee3cbcf5e2c7abd85d3c04bdba762
Haj B <phajib@gmail.com>
2020-03-14 04:00:26 -0700

    user controller set

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,30 @@
 class Api::V1::UsersController < ApplicationController
     def index
+        users = User.all
+        render json: UserSerializer.new(users)
     end
 
     def new
+        @user = User.new
     end
 
     def create
+        if @user.valid?
+            session[:current_user_id] = @user.id
+            render json: user, status: :created
+        else
+            render :new
+            render json: { error: 'ERROR: Failed to create User' }, status: :unprocessable_entity
+        end
     end
     
     def show
+        @user = User.find(params[:id])
+        render json: user#, include: [:movies]
     end
 
     private
         def user_params
+            params.require(:user).permit(:username, :password, :about, :profile_picture)
         end
 end
