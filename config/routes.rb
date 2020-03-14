1936eb38da3f26e7073628e04aad8e2dc849b035
Haj B <phajib@gmail.com>
2020-03-14 03:41:29 -0700

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :movie_genres
-  resources :genres
-  resources :movies
-  resources :user_movies
-  resources :users
+namespace :api do
+  namespace :v1 do
+    resources :users
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    post '/logout' => 'sessions#destroy'
+    get '/profile' => 'users#index'
+
+  end
+end
+
+
+  # resources :comments
+  # resources :movie_genres
+  # resources :genres
+  # resources :movies
+  # resources :user_movies
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
