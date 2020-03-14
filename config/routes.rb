4ab572f5f035b65830b732cd74bade6556b307e7
Haj B <phajib@gmail.com>
2020-03-14 02:43:16 -0700

    db setup user, move, user_movie, genre, movie_genre, comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :movie_genres
+  resources :genres
+  resources :movies
+  resources :user_movies
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
