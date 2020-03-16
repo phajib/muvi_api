9d9fb41188ef01823aae54f434fa1fd9664fac49
Haj B <phajib@gmail.com>
2020-03-16 00:53:29 -0700

    no need for username in comments

--- db/migrate/20200314093432_create_comments.rb
+++ db/migrate/20200314093432_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :username
       t.integer :user_id
       t.integer :movie_id
       t.string :content
