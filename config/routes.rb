6f8836148358f896d3fa8f7a84a08e7656294dc2
Haj B <phajib@gmail.com>
2020-03-16 08:02:51 -0700

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,37 @@
 Rails.application.routes.draw do
-namespace :api do
-  namespace :v1 do
-    resources :users
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    post '/logout' => 'sessions#destroy'
-    get '/profile' => 'users#index'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
 
+      get '/profile' => 'users#index'
+      
+      get '/movies' => 'movies#all_movies'
+      get '/movie/:id' => 'movies#movie_details'
+      get '/latest' => 'movies#latest_movies'
+      get '/upcoming' => 'movies#upcoming_movies'
+      get '/popular/:page' => 'movies#popular_movies'
+      get '/top_rated' => 'movies#top_rated_movies'
+    
+      get '/usermovies', to: 'user_movies#index'
+      post '/usermovies', to: 'user_movies#create'
+    
+      get '/comments', to: 'comment#index'
+      get '/comments/movie/:tmdb_id', to: 'comment#movie'
+      post '/comments', to: 'comment#create'
+      delete '/comments/:id', to: 'comment#destroy'
+    end
   end
-end
 
 
-  # resources :comments
-  # resources :movie_genres
-  # resources :genres
-  # resources :movies
-  # resources :user_movies
-  # resources :users
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+# namespace :api do
+#   namespace :v1 do
+    # resources :movies
+    # resources :user_movies
+    # resources :comments
+#   end
+# end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
