a1efab22fba243f3c183dd93ff0d68410a9712b6
Haj B <phajib@gmail.com>
2020-03-26 17:28:43 -0700

    fixed routes standards and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
-      
+      resources :users#, only: [:create]
       post '/login' => 'auth#create'
-
+      
+      get '/user/all_info' => 'users#allInfo'
+      get '/user/:id/info' => 'users#info'
       get '/profile' => 'users#profile'
+      patch '/user/edit' => "users#edit"
+
       # get '/userprofile' => 'users#profile'
       # get '/user' => 'users#show'
-      
+
       get '/movies' => 'movies#all_movies'
       get '/movie/:id' => 'movies#movie_details'
       get '/latest' => 'movies#latest_movies'
@@ -16,14 +19,14 @@ Rails.application.routes.draw do
       get '/popular/:page' => 'movies#popular_movies'
       get '/top_rated' => 'movies#top_rated_movies'
       get '/search/:orignal_title' => 'movies#search'
-    
-      get '/usermovies', to: 'user_movies#index'
-      post '/usermovies', to: 'user_movies#create'
-    
-      get '/comments', to: 'comments#index'
-      get '/comments/movie/:tmdb_id', to: 'comment#movie'
-      post '/comments', to: 'comments#create'
-      delete '/comments/:id', to: 'comments#destroy'
+      
+      get '/usermovies' => 'user_movies#index'
+      post '/usermovies' => 'user_movies#create'
+
+      get '/comments' => 'comments#index'
+      get '/comments/movie/:tmdb_id' => 'comment#movie'
+      post '/comments' => 'comments#create'
+      delete '/comments/:id' => 'comments#destroy'
     end
   end
 
