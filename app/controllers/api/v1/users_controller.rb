26929f7e4498c4930bc33fe713b138947cf4bc12
Haj B <phajib@gmail.com>
2020-03-26 17:30:17 -0700

    added info allinfo edit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :authenticate_user, only: [:create]
+    skip_before_action :authenticate_user, only: [:allInfo, :info, :create, :edit]
 
     # def index
     #     users = User.all
@@ -9,6 +9,29 @@ class Api::V1::UsersController < ApplicationController
     # def new
     #     @user = User.new
     # end
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
+    def allInfo 
+        user = User.find(@user.id)
+        if user
+            render json: {userMovies: user.movies}
+        else
+            render json: {error: 'ERROR: No User Datat' }, status: :not_acceptable
+        end
+    end
+
+    def info 
+        profile = User.find(params[:id])
+        if profile
+            render json: {user_info: profile, userMovies: profile.games, comments: profile.comments}
+            # render json: @user#, include: [:movies]   
+        else
+            render json: { error: 'ERROR: No User Data' }, status: :not_acceptable
+        end
+    end
 
     def create
         @user = User.create(user_params)
@@ -23,14 +46,21 @@ class Api::V1::UsersController < ApplicationController
         end
     end
     
+    def edit
+        user = User.find(@user.id)
+        if params[:password] == "" then
+            user.update(username: params[:user][:username], about: params[:user][:about], picture_profile: params[:user][:picture_profile])
+        else 
+            user.update(user_params)
+        end
+
+        render json: user.to_json
+    end
+
     # def show
     #     @user = User.find(params[:id])
     #     render json: @user#, include: [:movies]
     # end
-
-    def profile
-        render json: { user: UserSerializer.new(current_user) }, status: :accepted
-    end
 
     private
         def user_params
