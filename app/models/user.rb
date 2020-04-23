9cdbd58a5bd49386f51aa89ef8324c7b98951837
Haj B <phajib@gmail.com>
2020-04-23 01:03:11 -0700

    has many comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :user_movies
     has_many :movies, through: :user_movies
+    has_many :comments
     has_secure_password
     validates :username, uniqueness: true
 end
