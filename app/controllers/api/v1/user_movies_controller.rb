aae9d36b5a52d1e76ade0beabdfd421e32687570
Haj B <phajib@gmail.com>
2020-04-24 10:43:13 -0700

    changed params in create

--- app/controllers/api/v1/user_movies_controller.rb
+++ app/controllers/api/v1/user_movies_controller.rb
@@ -1,43 +1,39 @@
 class Api::V1::UserMoviesController < ApplicationController
-    before_action :authenticate_user, only: [:create, :index]
+  before_action :authenticate_user, only: [:create, :index]
 
-    def index
-        users_id = request.headers["User"]
-        find_user = User.find(users_id)
-        users_movies = find_user.movies
-        render json: users_movies
-    end
+  def index
+    users_id = request.headers["User"]
+    find_user = User.find(users_id)
+    users_movies = find_user.movies
+    render json: users_movies
+  end
 
-    def create
-      id = params[:movie][:id]
-      user = User.find_by(id: params[:user_id])
-      saved_movie_id = Movie.find_by(tmdb_id: id)
-
-      if saved_movie_id
-        movie_ID = Movie.where(tmdb_id: id)[0].id
-        if user.user_movies.where(movie_id: movie_ID).length > 0
-          render json: { message: 'You have this movie already!' }
-        else
-          render json: UserMovie.create(user_id: user.id, movie_id: movie_ID).movie
-        end
+  def create
+    if Movie.find_by(tmdb_id: params[:movie][:id])
+      movie_ID = Movie.where(tmdb_id: params[:movie][:id])[0].id
+      if @user.user_movies.where(movie_id: movie_ID).length > 0
+        render json: { message: 'You have this movie already!' }
       else
-        @add_movie = Movie.create_or_find_by(  #create_or_find_by! Will raise if validation error
-          popularity: params[:movie]['popularity'],
-          vote_count: params[:movie]['vote_count'],
-          video: params[:movie]['video'],
-          poster_path: params[:movie]['poster_path'],
-          homepage: params[:movie]['homepage'],
-          tmdb_id: params[:movie]['id'],
-          imdb_id: params[:movie]['imdb_id'],
-          backdrop_path: params[:movie]['backdrop_path'],
-          original_title: params[:movie]['original_title'],
-          vote_average: params[:movie]['vote_average'],
-          overview: params[:movie]['overview'],
-          release_date: params[:movie]['release_date'],
-          runtime: params[:movie]['runtime'],
-          tagline: params[:movie]['tagline']
-        )
-        render json: UserMovie.create(user_id: user.id, movie_id: @add_movie.id).movie
+        render json: UserMovie.create(user_id: @user.id, movie_id: movie_ID).movie
       end
+    else
+      @add_movie = Movie.create_or_find_by(  #create_or_find_by! Will raise if validation error
+        popularity: params[:movie]['popularity'],
+        vote_count: params[:movie]['vote_count'],
+        video: params[:movie]['video'],
+        poster_path: params[:movie]['poster_path'],
+        homepage: params[:movie]['homepage'],
+        tmdb_id: params[:movie]['id'],
+        imdb_id: params[:movie]['imdb_id'],
+        backdrop_path: params[:movie]['backdrop_path'],
+        original_title: params[:movie]['original_title'],
+        vote_average: params[:movie]['vote_average'],
+        overview: params[:movie]['overview'],
+        release_date: params[:movie]['release_date'],
+        runtime: params[:movie]['runtime'],
+        tagline: params[:movie]['tagline']
+      )
+      render json: UserMovie.create(user_id: @user.id, movie_id: @add_movie.id).movie
     end
+  end
 end
